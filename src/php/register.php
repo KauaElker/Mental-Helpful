2a2162c2e5f178d37322b6d9e127573d4709d41c
KauaElker <kauaelker@gmail.com>
2024-12-04 11:15:00 -0300

    pospda

--- src/php/register.php
+++ src/php/register.php
@@ -1,17 +1,32 @@
 <?php
-include 'db_connection.php';
+// Configuração de conexão com o banco de dados
+$servername = "localhost";
+$username = "root"; // usuário padrão do MySQL
+$password = "";     // senha padrão do MySQL (em branco)
+$dbname = "mental_helpful"; // nome do banco de dados
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Conta criada com sucesso!";
-    } else {
-        echo "Erro: " . $sql . "<br>" . $conn->error;
-    }
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
 }
+
+// Recebe os dados do formulário
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Criptografa a senha
+
+// Insere os dados no banco de dados
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Conta criada com sucesso!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+// Fecha a conexão
+$conn->close();
 ?>
