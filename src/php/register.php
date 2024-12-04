117bdf4d206da80a1be0a2539b2cc915387aab84
KauaElker <kauaelker@gmail.com>
2024-12-04 10:22:13 -0300

    q
    q

---
+++ src/php/register.php
@@ -0,0 +1,17 @@
+<?php
+include 'db_connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Conta criada com sucesso!";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
